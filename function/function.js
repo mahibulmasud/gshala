4d7ae3630e57e9126621e21a6e7b1c7dafda8392
Masud <mahibulmasud@gmail.com>
2022-01-10 09:29:37 +0600

    edited

--- function/function.js
+++ function/function.js
@@ -1,3 +1,20 @@
+let carouselIndicatorButtons = document.querySelectorAll('#custom-carousel-indicators button')
+document.querySelectorAll('.carousel').forEach((button) => {
+    button.addEventListener('slid.bs.carousel', function () {
+        let activeIndicator = document.querySelector('.carousel-indicators button.active')
+
+        carouselIndicatorButtons.forEach((button2) => {
+            button2.classList.remove('active')
+        })
+
+        document.querySelector('#custom-carousel-indicators button[data-bs-slide-to="'
+            + activeIndicator.dataset.bsSlideTo + '"]').classList.add('active')
+    });
+});
+
+
+
+
 const nextIcon = '<img src="../images/prev-btn.png" class="nexticon np-icon"/>  ';
 const prevIcon = '<img src="../images/prev-btn.png" class="previcon np-icon"/> ';
 
